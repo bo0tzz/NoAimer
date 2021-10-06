b0906400dedc89bd46892edd12656e65d2a35d04
bo0tzz <btdewilligen@gmail.com>
2021-10-06 13:34:34 +0200

    BepInEx boilerplate

--- NoAimer/NoAimer.cs
+++ NoAimer/NoAimer.cs
@@ -1,8 +1,21 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace NoAimer
 {
+    [BepInPlugin(modGuid, modName, modVersion)]
+    [BepInProcess("Peglin.exe")]
     public class NoAimer : BaseUnityPlugin
     {
+        private const string modGuid = "me.bo0tzz.peglinmods.noaimer";
+        private const string modName = "No Aimer";
+        private const string modVersion = "1.0.0.0";
+        private readonly Harmony harmony = new Harmony(modGuid);
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo("No Aimer mod loaded");
+        }
     }
 }
